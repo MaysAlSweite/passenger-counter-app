6e2d2ea8c73f4ec0aa9c268f83c5c63cda0a38aa
Mays <mayssweite@gmail.com>
2025-05-17 14:32:21 +0300

    Save button is added

--- src/script.js
+++ src/script.js
@@ -1,9 +1,18 @@
 const body = document.getElementById("body");
 const increment = document.getElementById("incrBtn");
+const save = document.getElementById("saveBtn");
 const counter = document.getElementById("counter");
+const entries = document.getElementById("entries");
 let count=0;
 
 function incrementing () {
     count++;
     counter.textContent = count;
+}
+function saving () {
+    let str=count+" - ";
+    entries.textContent += str;
+    count=0;
+    counter.textContent = count;
+
 }
